1f64c6a9a6567e17dcfc0e29a6d00c77df33bf6a
kegedev <kemalgediogludev@gmail.com>
2025-02-04 18:59:51 +0300

    Protection added

--- Assets/Scripts/Game/Snake/SnakeSegment.cs
+++ Assets/Scripts/Game/Snake/SnakeSegment.cs
@@ -5,23 +5,68 @@ public class SnakeSegment : MonoBehaviour
 {
     public PoolType PoolType;
     [SerializeField] private HealthComponent _healthComponent;
-
+    [SerializeField] private ParticleSystem protection;
     [SerializeField] GameObject KillParticle;
-
+    [SerializeField] float ProtectionTime;
+    private float timeElapsed = 0f;
+    private bool isTimerRunning = false;
     private void Start()
     {
         ActionManager.Attack += GetDamage;
+        ActivationProtectionFX(false);
     }
 
+    private void Update()
+    {
+        if (isTimerRunning)
+        {
+            timeElapsed += Time.deltaTime;
 
- 
+            if (timeElapsed >= ProtectionTime)
+            {
+                isTimerRunning = false;
+                timeElapsed = 0f;
+
+                OnTimerCompleted();
+            }
+        }
+    }
+    public void OnTimerCompleted()
+    {
+        StopTimer();
+        ResetTimer();
+        ActivationProtectionFX(false);
+    }
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+        timeElapsed = 0f;
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+        timeElapsed = 0f;
+    }
+
+    public void ResetTimer()
+    {
+        timeElapsed = 0f;
+    }
     private void GetDamage(SnakeSegment segment, int damage)
     {
-        if (segment == this)
+        if (!isTimerRunning && segment == this)
         {
             _healthComponent.TakeDamage(damage);
-           
+            ActivationProtectionFX(true);
+            StartTimer();
         }
+    }
+
+    public void ActivationProtectionFX(bool isActive)
+    {
+        if(isActive) protection.Play();
+        else protection.Stop();
     }
 
     internal void Kill()
